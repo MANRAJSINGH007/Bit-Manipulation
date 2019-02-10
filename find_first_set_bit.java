ec7ba25d93740db5da19b6a64013ed6f4d4a25e7
MANRAJSINGH007 <35843207+MANRAJSINGH007@users.noreply.github.com>
2019-02-10 22:32:39 +0530

    Create find_first_set_bit.java

---
+++ find_first_set_bit.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+	
+	public static void main(String[] args) {
+		/* Your class should be named Main
+		 * Read input as specified in the question.
+		 * Print output as specified in the question.
+		*/
+      Scanner sc=new Scanner(System.in);
+      int n=sc.nextInt();
+      
+      int temp=n&(n-1);
+      
+      System.out.println(n-temp);
+      
+      
+	}
+}
